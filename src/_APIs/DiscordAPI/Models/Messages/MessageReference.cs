41c1c99411160c74aaa93ba439dbb1b5ab85caeb
matthew4850 <40522069+matthew4850@users.noreply.github.com>
2020-11-21 14:37:46 +0000

    Create MessageReference class.

---
+++ src/_APIs/DiscordAPI/Models/Messages/MessageReference.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordAPI.Models.Messages
+{
+    /// <summary>
+    /// A model for a message reference.
+    /// </summary>
+    public class MessageReference
+    {
+        /// <summary>
+        /// Gets or sets the id of the message.
+        /// </summary>
+        [JsonProperty("message_id")]
+        public string MessageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id of the channel the message originates from.
+        /// </summary>
+        [JsonProperty("channel_id")]
+        public string ChannelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id of the guild the message originates from.
+        /// </summary>
+        [JsonProperty("guild_id")]
+        public string GuildId { get; set; }
+    }
+}
